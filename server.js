4f19ed627d60f728f5d031897ab1ec2124f4e34e
Andres Rodriguez <banrostin@gmail.com>
2024-10-02 13:35:51 -0600

    Cors all

--- server.js
+++ server.js
@@ -2,31 +2,25 @@ const express = require("express");
 const morgan = require("morgan");
 const router = require('./src/routes');
 const movieServices = require("./src/services/movieServices");
-const allowedOrigins = [
-  process.env.FRONTEND_URL, 
-  "https://app-cinema-front-1yjffgi0p-andresrs-projects.vercel.app"
-];
 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-  }
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
-  
+
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(router);
 
-app.post("/movies",async (req,res)=>{
+app.post("/movies", async (req, res) => {
   const { title, year, director, duration, genre, rate, poster } = req.body;
-  
+
   try {
-    const newMovie = await movieServices.createMovies({title,year,director,duration,genre,rate,poster});
+    const newMovie = await movieServices.createMovies({ title, year, director, duration, genre, rate, poster });
     res.status(201).json(newMovie);
   } catch (error) {
     console.error(error);
@@ -34,9 +28,9 @@ app.post("/movies",async (req,res)=>{
   }
 })
 
-app.use((err,req,res,next) =>{
-  res.status(err.statusCode||500).json({error:err.message})
-}) 
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({ error: err.message })
+})
 
 
 
